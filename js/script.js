dbc27348a35d7e36ee6a0ae0f9eca1116897648d
Corey <corey.carmichael@gmail.com>
2023-05-06 21:45:06 -0400

    add search functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 const global = {
-    currentPage: window.location.pathname
+    currentPage: window.location.pathname,
+    search: {
+      term: '',
+      type: '',
+      page: 1,
+      totalPages: 1,
+    },
+    api: {
+      apiKey: '3f929fc2bcfa67f7d4cdeb5bd1bcd53a',
+      apiUrl: 'https://api.themoviedb.org/3/'
+    }
 }
 
 // Display 20 most popular movies
@@ -236,6 +246,22 @@ function displayBackgroundImage(type, backgroundPath) {
     };
 }
 
+// Search
+async function search () {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    const results = await searchAPIData();
+    console.log(results);
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+ 
 // Display slider movies
 async function displaySlider () {
     const { results } = await fetchAPIData('movie/now_playing');
@@ -284,8 +310,8 @@ function initSwiper () {
 
 // Fetch data
 async function fetchAPIData(endpoint) {
-    const API_KEY = '3f929fc2bcfa67f7d4cdeb5bd1bcd53a';
-    const API_URL = 'https://api.themoviedb.org/3/';
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiUrl;
 
     showSpinner();
 
@@ -296,6 +322,22 @@ async function fetchAPIData(endpoint) {
     hideSpinner();
 
     return data;
+}
+
+// Make request to search
+async function searchAPIData(endpoint) {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-us&query=${global.search.term}`);
+
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
 }
 
 function showSpinner() {
@@ -315,6 +357,17 @@ function highlightActiveLink() {
         }
     });
 }
+
+// Show alert
+function showAlert(message, className) {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+}
+
 
 function addCommasToNumber(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -338,7 +391,7 @@ function init() {
             displayShowDetails();
             break;
         case '/search.html':
-            console.log('Search');
+            search();
             break;
     }
 
